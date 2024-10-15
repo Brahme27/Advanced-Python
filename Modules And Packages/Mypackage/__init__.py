d84b925afbc2a37f08dcf9c42676d89f51befd2a
Brahme27 <zaynbrahme@gmail.com>
2024-10-15 14:45:39 +0530

    Modules,Packages And Standard Libraries

---
+++ Modules And Packages/Mypackage/__init__.py
@@ -0,0 +1,15 @@
+''' 
+my_package/
+    __init__.py
+    module1.py
+    module2.py
+
+'''
+'''
+The __init__.py file is created in a package to initialize 
+the package and tell Python that the directory 
+should be treated as a package. Without it, 
+Python wouldn’t recognize the folder as a 
+valid Python package, and you wouldn’t be able 
+to import modules from it properly.
+'''
---
+++ Modules And Packages/Mypackage/test.py
@@ -0,0 +1,3 @@
+from Mypackage import maths
+print(maths.addition(10,20))
+print(maths.subraction(10,20))
